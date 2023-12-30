239d915cfb02d26b2ebde1dc29c7aee17d31bb93
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-12-31 02:38:28 +0530

    feat: add kafka service

---
+++ apps/server/src/services/kafka.ts
@@ -0,0 +1,62 @@
+import { Kafka, Producer } from "kafkajs";
+import fs from "fs";
+import path from "path";
+import prismaClient from "./prisma";
+
+const kafka = new Kafka({
+    brokers: ["kafka-1f216242-shubh-5050.a.aivencloud.com:27683"],
+    ssl:{
+        ca: [fs.readFileSync(path.resolve('./ca.pem'), "utf-8")],
+    },
+    sasl:{
+        username: 'avnadmin',
+        password: 'AVNS_5sdKYV5s0wq8DpwiBtq',
+        mechanism: 'plain',
+    }
+  })
+
+let producer: null | Producer = null;
+export async function createProducer() {
+    if(producer) return producer;
+    const _producer = kafka.producer()
+    await _producer.connect();
+    producer = _producer;
+    return producer;
+  }
+export async function produceMessage(message:string) {
+    const producer = await createProducer();
+    await producer.send({
+        messages: [{key: `message-${Date.now()}`, value: message}],
+        topic:"MESSAGES",
+    });
+    return true;
+}
+
+export async function startMessageConsumer() {  
+    const consumer = kafka.consumer({groupId:  "default"});
+    await consumer.connect();
+    await consumer.subscribe({topic: "MESSAGES", fromBeginning: true});
+
+    await consumer.run({
+        autoCommit: true,
+        eachMessage: async ({message, pause}) => {
+            if (!message.value) return;
+            console.log('New message recv...');
+            try{
+                await prismaClient.message.create({
+                    data:{
+                        text: message.value?.toString(),
+                    },
+                })
+            } catch(err){
+                console.log('Something went wrong');
+                pause()
+                setTimeout(() => {
+                    consumer.resume([{topic: "MESSAGES"}])
+                }, 60*1000)
+
+            }
+        }
+    });
+}
+export default kafka;
--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,5 +1,6 @@
 import {Server} from 'socket.io'
 import { Redis } from 'ioredis';
+import { produceMessage } from './kafka';
 
 const pub = new Redis({
     host:'redis-20ad081e-shubh-5050.a.aivencloud.com',
@@ -39,9 +40,11 @@ class SocketService{
                 await pub.publish('MESSAGES',JSON.stringify({message}));
             })
         });
-        sub.on('message',(channel,message)=>{
+        sub.on('message',async (channel,message)=>{
             if(channel === "MESSAGES"){
                 io.emit("message", message);
+                await produceMessage(message);
+                console.log("Message Produced to Kafka Broker");
             }
         })
         
