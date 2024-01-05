27defe5e3ed26bf3c9711ea048dbf2a807bf55d0
shubhjena <117462852+shubhjena@users.noreply.github.com>
2024-01-05 23:48:39 +0530

    refactor: update prisma schema

--- apps/server/prisma/migrations/20240103200123_init/migration.sql
+++ apps/server/prisma/migrations/20240103200123_init/migration.sql
@@ -2,7 +2,9 @@
 CREATE TABLE "messages" (
     "id" TEXT NOT NULL,
     "text" TEXT NOT NULL,
+    "sender" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
 );
---
+++ apps/server/prisma/migrations/20240104180943_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "messages" ADD COLUMN     "chatId" TEXT NOT NULL DEFAULT 'Chat 1';
