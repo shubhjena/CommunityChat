0a1d0eebc2cc07ed7ff4b1417402fc50ae0ff936
shubhjena <117462852+shubhjena@users.noreply.github.com>
2024-01-23 22:30:42 +0530

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,105 +1,76 @@
 "use client";
-import React, { useState, useEffect, useRef } from "react";
-import { useSocket } from "../context/SocketProvider";
-import Message from "../components/Message";
+import React, { useEffect } from "react";
+import Empty from "../components/Empty";
+import ChatList from "../components/ChatList";
+import { useStateProvider } from "../context/StateProvider";
+import Chat from "../components/Chat/Chat";
+import Message from "../components/Chat/Message";
+import shortid from "shortid";
 
-// Function to format the date
-const formatDate = (date: Date): string => {
-  const options: Intl.DateTimeFormatOptions = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-  return new Intl.DateTimeFormat("en-US", options).format(date);
-};
+export default function page() {
+  const [{ selectedChat }, dispatch] = useStateProvider();
 
-// Function to check if two dates are on the same day
-const isSameDay = (date1: Date, date2: Date): boolean => {
-  return date1.toDateString() === date2.toDateString();
-};
-
-// Function to check if two dates are on the same day
-const isSameId = (user1: string, user2: string): boolean => {
-  return user1 === user2;
-};
-
-export default function Page() {
-  const { sendMessage, messages, userId } = useSocket();
-  const [input, setInput] = useState("");
-  const messagesContainerRef = useRef<HTMLDivElement | null>(null);
-
-  // Function to handle sending a message
-  const handleSendMessage = () => {
-    if (input.trim() !== "") {
-      sendMessage({ text: input, sender: userId, sendTime: new Date() });
-      setInput(""); // Reset the input after sending the message
+  const parseMessage = (rawMessage: {
+    id: string;
+    text: string;
+    sender: string;
+    createdAt: string;
+    updatedAt: string;
+    chatId: string;
+  }): Message => {
+    try {
+      return {
+        text: rawMessage.text,
+        sender: rawMessage.sender,
+        sendTime: new Date(rawMessage.updatedAt),
+        chatId: rawMessage.chatId,
+      };
+    } catch (error) {
+      console.error("Error parsing message:", error);
+      // Handle the error, or return a default message
+      return {
+        text: "Error parsing message",
+        sender: "Unknown sender",
+        sendTime: new Date(),
+        chatId: rawMessage.chatId,
+      };
     }
   };
 
-  // Function to handle Enter key press
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      handleSendMessage();
-    }
-  };
-
-  // Effect to scroll to the bottom when messages change
   useEffect(() => {
-    if (messagesContainerRef.current) {
-      messagesContainerRef.current.scrollTop =
-        messagesContainerRef.current.scrollHeight;
+    const getMessages = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8000/messages?chatId=${selectedChat}`
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch messages");
+        }
+        const messagesData = await response.json();
+        const parsedMessages = messagesData.map(parseMessage);
+        dispatch({ type: "SET_MESSAGES", payload: parsedMessages });
+        // setMessages(parsedMessages);
+      } catch (error) {
+        console.error("Error fetching messages:", error);
+      }
+    };
+    if (selectedChat) getMessages();
+  }, [selectedChat]);
+
+  useEffect(() => {
+    //get userid from local storage
+    let storedUserId = localStorage.getItem("userId");
+    if (!storedUserId) {
+      storedUserId = shortid.generate();
+      localStorage.setItem("userId", storedUserId);
     }
-  }, [messages]);
+    dispatch({ type: "SET_USER_ID", payload: storedUserId });
+  }, []);
 
   return (
-    <div className="max-w-3xl h-screen mx-auto px-16 bg-zinc-700 flex flex-col">
-      <div
-        className="flex-1 overflow-y-auto mb-0.5 pt-4 px-4 flex flex-col custom-scrollbar"
-        ref={messagesContainerRef}
-      >
-        {messages.map((message, index) => (
-          <div key={index} className="flex flex-col">
-            {index === 0 ||
-            !isSameDay(
-              new Date(message.sendTime),
-              new Date(messages[index - 1].sendTime)
-            ) ? (
-              // Display date if it's a new day
-              <p className="mx-auto text-center text-xxs text-gray-400 mt-2 mb-1.5 bg-zinc-900 p-1 px-2 rounded-md bg-opacity-75 shadow">
-                {formatDate(new Date(message.sendTime))}
-              </p>
-            ) : null}
-            {/* Render individual message */}
-            {index === 0 || !isSameId(message.sender, messages[index - 1].sender) ? (
-              <Message key={index} message={message} userId={userId} sameId={false}/>
-            ) : (
-              <Message key={index} message={message} userId={userId} sameId={true}/>
-            )}
-          </div>
-        ))}
-        <div className={messages.length === 0 ? "block" : "hidden"}>
-          Your messages will appear here....
-        </div>
-      </div>
-      {/* Input and send button */}
-      <div className="flex items-center pb-8">
-        <input
-          type="text"
-          className="flex-1 p-4 border border-r-0 bg-zinc-700 border-gray-500 rounded-l-2xl outline-none"
-          placeholder="Type your message..."
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onKeyDown={handleKeyDown}
-        />
-        <button
-          className="p-4 bg-zinc-700 hover:bg-zinc-600 border border-gray-500 rounded-r-2xl text-lime-500 transition-all duration-100"
-          onClick={handleSendMessage}
-        >
-          Send
-        </button>
-      </div>
+    <div className={`grid grid-cols-[1fr,3fr] h-screen max-h-screen`}>
+      <ChatList />
+      {selectedChat ? <Chat chatId={selectedChat} /> : <Empty />}
     </div>
   );
 }
